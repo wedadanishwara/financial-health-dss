ba01925da19245084638b169f91e92eea26a87cc
Weda Danishwara <wedadanish29@gmail.com>
2026-09-14 22:57:10 +0800

    Export User model from __init__.py
    Added User model to the module's exports.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
+from app.models.user import User
 import os
+
+__all__ = ['User']
 
 load_dotenv()
 
