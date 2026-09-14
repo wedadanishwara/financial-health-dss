d5eab79cf58af94c538f5523c4f8830611e45b66
Weda Danishwara <wedadanish29@gmail.com>
2026-09-14 23:03:11 +0800

    Add run.py to start Flask application

---
+++ backend/run.py
@@ -0,0 +1,8 @@
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    debug = os.getenv('FLASK_ENV') == 'development'
+    app.run(debug=debug, host='0.0.0.0', port=5000)
